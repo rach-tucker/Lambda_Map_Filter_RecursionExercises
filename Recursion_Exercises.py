e7c38e966d50b2735d0b02d23f16caf19ae948f2
Rachel Tucker <rach.mello@hotmail.com>
2022-12-28 19:57:13 -0600

    first commit

---
+++ Recursion_Exercises.py
@@ -0,0 +1,40 @@
+#Exercise #1
+#Filter out all of the empty strings from the list below
+#Output: ['Argentina', 'San Diego', 'Boston', 'New York']
+places = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
+
+
+
+
+
+
+#Exercise #2
+#Write an anonymous function that sorts this list by the last name...
+#Hint: Use the ".sort()" method and access the key"
+#Output: ['Victor aNisimov', 'Gary A.J. Bernstein', 'Joel Carter', 'Andrew P. Garfield', 'David hassELHOFF']
+
+
+authors = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
+
+
+
+#Exercise #3
+#Convert the list below from Celsius to Farhenheit, using the map function with a lambda...
+
+#Output: [('Nashua', 89.6), ('Boston', 53.6), ('Los Angeles', 111.2), ('Miami', 84.2)]
+
+# F = (9/5)*C + 32
+places = [('Nashua',32),("Boston",12),("Los Angeles",44),("Miami",29)]
+
+
+
+
+#Exercise #4
+#Write a recursion function to perform the fibonacci sequence up to the number passed in.
+#Output for fib(5) => Iteration 0: 0 Iteration 1: 1 Iteration 2: 1 Iteration 3: 2 Iteration 4: 3 Iteration 5: 5 Iteration 6: 8
+
+
+
+
+#Exercise #5
+#Create a generator that takes a number argument and yields that number squared, then prints each number squared until zero is reached.
