d7ca90fb2f95e01ecd480bdd71dbbd7ca9bfd4d3
rach-tucker <118758075+rach-tucker@users.noreply.github.com>
2022-12-31 20:18:36 -0700

    Update Recursion_Exercises.py

--- Recursion_Exercises.py
+++ Recursion_Exercises.py
@@ -3,7 +3,7 @@
 #Output: ['Argentina', 'San Diego', 'Boston', 'New York']
 places = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
 
-
+print(list(filter(lambda city: city.split(), places)))
 
 
 
@@ -16,6 +16,7 @@ places = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
 
 authors = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
 
+authors.sort(key = lambda x: x.lower().split()[-1])
 
 
 #Exercise #3
@@ -27,14 +28,34 @@ authors = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELH
 places = [('Nashua',32),("Boston",12),("Los Angeles",44),("Miami",29)]
 
 
-
+convert_farhenheit = list(map(lambda temp: (temp[0], temp[1] * 9/5 + 32), places))
+print(convert_farhenheit)
 
 #Exercise #4
 #Write a recursion function to perform the fibonacci sequence up to the number passed in.
 #Output for fib(5) => Iteration 0: 0 Iteration 1: 1 Iteration 2: 1 Iteration 3: 2 Iteration 4: 3 Iteration 5: 5 Iteration 6: 8
 
 
+def fib_iter(num):
+    previous = 1
+    current = 0
+    i = 0
+    while i < num:
+        yield current 
+        previous, current = current, previous + current 
+        i += num
+
+for i,f in enumerate(fib_iter(20)):
+    print(f"iteration {i}: {f}")
 
 
 #Exercise #5
 #Create a generator that takes a number argument and yields that number squared, then prints each number squared until zero is reached.
+
+def num_squared(number):
+    while number != 0:
+        yield number **2
+        number -= 1
+
+for x in num_squared(5):
+    print(x)
